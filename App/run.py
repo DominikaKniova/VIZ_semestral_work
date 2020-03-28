8cb57346f891d6ddac32580c99b3138576b9c705
pultarmi <milan.pultar@gmail.com>
2020-03-28 09:54:33 +0100

    linear mapping; minor css changes

--- App/run.py
+++ App/run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, send_file
 import numpy as np
-import csv
+import csv, os
 
 def generate_2d_points(low, high, n):
     return np.random.uniform(low=low, high=high, size=(n, 2))
@@ -14,30 +14,44 @@ def cart2pol(x, y):
     phi = np.arctan2(y, x)
     return(rho, phi)
 
-def pol2cart(rho, phi):
-    x = rho * np.cos(phi)
-    y = rho * np.sin(phi)
+def pol2cart(r, phi):
+    x = r * np.cos(phi)
+    y = r * np.sin(phi)
     return(x, y)
 
 @app.route('/')
 def index():
     data = generate_2d_points(10, 450, 200)
-    with open('data/points.csv', 'wb') as f:
+    with open('Tmp/points.csv', 'wb') as f:
         f.write(b'x,y\n')
         np.savetxt(f, data, delimiter=',', fmt='%.2f')
-    return render_template("simple.html")
+    return render_template("index.html")
 
-@app.route('/data/points.csv', methods=['GET'])
+@app.route('/Tmp/points.csv', methods=['GET'])
 def get_data():
-    return send_file('data/points.csv',
+    return send_file('Tmp/points.csv',
                      mimetype='text/csv',
                      attachment_filename='points.csv',
                      as_attachment=True)
 
-# @app.route('/data/marvel.json', methods=['GET'])
+@app.route('/Data/vectors.csv', methods=['GET'])
+def get_data2():
+    return send_file('Data/vectors.csv',
+                     mimetype='text/csv',
+                     attachment_filename='vectors.csv',
+                     as_attachment=True)
+
+@app.route('/Data/points.csv', methods=['GET'])
+def get_data3():
+    return send_file('Data/points.csv',
+                     mimetype='text/csv',
+                     attachment_filename='points.csv',
+                     as_attachment=True)
+
+# @App.route('/Tmp/marvel.json', methods=['GET'])
 # def get_data22():
 #     print('fafs')
-#     return send_file('data/marvel.json',
+#     return send_file('Tmp/marvel.json',
 #                      mimetype='text/json',
 #                      attachment_filename='marvel.json',
 #                      as_attachment=True)
